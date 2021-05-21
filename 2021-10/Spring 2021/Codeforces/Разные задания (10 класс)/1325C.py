cee7968cce0bef04d93ec35c2a129835dc98b470
PhilipEvdokimoff <philipp.evdokimov@icloud.com>
2021-05-21 23:45:52 +0300

    Удалил нерабочие программы

--- 2021-10/Spring 2021/Codeforces/Разные задания (10 класс)/1325C.py
+++
@@ -1,8 +0,0 @@
-# Codeforces 1325C: Ехаб и неПутевые МЕХы
-
-n = int(input())
-tree = []
-for i in range(n):
-    a, b = list(map(int, input().split()))
-    tree.append((a,b))
-print(tree)
--- 2021-10/Spring 2021/Codeforces/Разные задания (10 класс)/1461B.py
+++
@@ -1,9 +0,0 @@
-# Codeforces 1461B: Найди ель
-
-t = int(input())
-s = []
-for i in range(t):
-    n, m = list(map(int, input().split()))
-    for j in range(m):
-        r = []
-        r.append() 
